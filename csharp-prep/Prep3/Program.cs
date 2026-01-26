d443c6d068e5d90ade915d278b75a11817f864a0
Johngoetz24 <Johngoetz24@gmail.com>
2026-01-26 15:45:29 -0700

    So I hope This is the right way to psh things to get hub

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       int Magic_Number = Collect_Random_Number();
+       Game_Play(Magic_Number);
     }
+    static int Collect_Random_Number()
+    {
+        Console.Write("Please enter in a Magic Number:");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        return number;
+    }
+    static void Game_Play(int Magic_Number)
+    {
+            int Number_To_Guess = Magic_Number;
+            int Guessed_Number = 0;
+            while (Guessed_Number != Number_To_Guess){
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            Guessed_Number = int.Parse(userInput);
+            if (Guessed_Number < Magic_Number)
+            {
+                Console.Write("Higher");
+                Console.WriteLine();
+            }
+            if (Guessed_Number > Magic_Number)
+            {
+                Console.Write("Lower");
+                Console.WriteLine();
+            }
+        }
+        Console.Write("You Guessed it!");
+    }
+
+    
+
 }
